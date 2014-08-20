e9a89669834555d4dbae8682f9de2d5cf698d0d9
Daniel Thiel <daniel.thiel@happyfloat.com>
2014-08-20 20:16:49 +0200

    added migration

--- manage.py
+++ manage.py
@@ -4,10 +4,24 @@
 import os
 
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 from app import create_app, db
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+
+""" enable migration / alembic for sqlalchemy:
+    first time using it:
+        python manage.py db init
+        # all generated files must be added to version control !
+    how to create automati migration script:
+        python manage.py db migrate -m "message"
+    deploy the migration:
+        python manage.py db upgrade
+"""
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
